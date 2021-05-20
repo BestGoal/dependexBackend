80b6d4edd4e2c7a215234d69763ac6198f76ee4d
winner108 <73909926+winner108@users.noreply.github.com>
2021-05-20 17:27:10 +0300

    update

--- serverConf.js
+++ serverConf.js
@@ -2,7 +2,6 @@ module.exports = {
     "BASEURL" : __dirname+"/uploads/",
     "DIR" : __dirname,
     "ADMINPASSMETHOD" : "admin",
-    // "DBCONNECT":"mongodb://localhost:27017/8866casino",
     "ServerPort" : 3000,
     "session" : {
         expiretime : 1000 * 60 * 60
